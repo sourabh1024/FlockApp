fa644f23541294f003007c5480e6a419166e65d8
Sourabh.Suman <sourabh.su@media.net>
2017-01-15 02:02:18 +0530

    Added Widget event listener

--- src/Servlet/Widgets.java
+++ src/Servlet/Widgets.java
@@ -1,7 +1,32 @@
 package Servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by sourabh.su on 14/01/17.
  */
-public class Widgets {
+public class Widgets extends HttpServlet {
+
+    protected void getResponse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(200);
+        resp.setContentType("text/html");
+        out.println("<h1> Hello there </h1>");
+        out.flush();
+        out.close();
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getResponse(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getResponse(req, resp);
+    }
 }
